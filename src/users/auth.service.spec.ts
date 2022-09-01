80e0fca3ccdb3bd8c714268563d9fd49df73d9d4
egimenos <ergisan@gmail.com>
2022-09-01 13:59:25 +0200

    Improve auth service test file organization

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,22 +3,25 @@ import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
 
-it('can create an instance of auth service', async () => {
-  //crrate a fake copy of users service
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise<User[]>.resolve([]),
+      create: (email: string, password: string) =>
+        Promise<User>.resolve({ id: 1, email, password } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUsersService },
+      ],
+    }).compile();
 
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise<User[]>.resolve([]),
-    create: (email: string, password: string) =>
-      Promise<User>.resolve({ id: 1, email, password } as User),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: fakeUsersService },
-    ],
-  }).compile();
+    service = module.get(AuthService);
+  });
 
-  const service = module.get(AuthService);
-
-  expect(service).toBeDefined();
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
