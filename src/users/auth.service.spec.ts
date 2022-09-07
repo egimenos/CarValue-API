a0ec16498ec218714c41f3957a0f13f1ce7baf5c
egimenos <ergisan@gmail.com>
2022-09-07 13:01:52 +0200

    Add specs for signup method

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,8 +5,10 @@ import { User } from './users.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise<User[]>.resolve([]),
       create: (email: string, password: string) =>
         Promise<User>.resolve({ id: 1, email, password } as User),
@@ -23,5 +25,21 @@ describe('AuthService', () => {
 
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed pwd', async () => {
+    const user = await service.signup('test@test.com', 'test');
+    expect(user.password).not.toEqual('test');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      'email in use',
+    );
   });
 });
